7a718fb3009b33b741f61ad761a2473ec3c7cd86
AmberBit <hubert.lepicki@amberbit.com>
2009-03-23 10:33:13 +0100

    Initial version added

---
+++ generators/amber_bit_scaffold/templates/controller.rb
@@ -0,0 +1,53 @@
+class <%= controller_class_name %>Controller < ApplicationController
+  # GET /<%= plural_name %>
+  def index
+    @<%= plural_name %> = <%= class_name %>.paginate(:page => params[:page], :per_page => 10)
+  end
+
+  # GET /<%= plural_name %>/1
+  def show
+    @<%= singular_name %> = <%= class_name %>.find(params[:id])
+  end
+
+  # GET /<%= plural_name %>/new
+  def new
+    @<%= singular_name %> = <%= class_name %>.new
+  end
+
+  # GET /<%= plural_name %>/1/edit
+  def edit
+    @<%= singular_name %> = <%= class_name %>.find(params[:id])
+  end
+
+  # POST /<%= plural_name %>
+  def create
+    @<%= singular_name %> = <%= class_name %>.new(params[:<%= singular_name %>])
+
+    if @<%= singular_name %>.save
+      flash[:notice] = t('<%= singular_name %>.flash.saved')
+      redirect_to @<%= singular_name %>
+    else
+      render :action => 'new'
+    end
+  end
+
+  # PUT /<%= plural_name %>/1
+  def update
+    @<%= singular_name %> = <%= class_name %>.find(params[:id])
+
+    if @<%= singular_name %>.update_attributes(params[:<%= singular_name %>])
+      flash[:notice] = t('<%= singular_name %>.flash.saved')
+      redirect_to @<%= singular_name %>
+    else
+      render :action => 'edit'
+    end
+  end
+
+  # DELETE /<%= plural_name %>/1
+  def destroy
+    @<%= singular_name %> = <%= class_name %>.find(params[:id])
+    @<%= singular_name %>.destroy
+    flash[:notice] = t('<%= singular_name %>.flash.deleted')
+    redirect_to <%= plural_name %>_path
+  end
+end
---
+++ tasks/amber_bit_scaffold_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :amber_bit_scaffold do
+#   # Task goes here
+# end
